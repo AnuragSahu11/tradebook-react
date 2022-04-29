6485c74791103b7771ea4e097292cdc775f37b8c
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-04-29 18:37:12 +0530

    [feat]Added landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { LandingPage } from "./components/index";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,1 @@
+export { LandingPage } from "./landing-Page/landing-page";
