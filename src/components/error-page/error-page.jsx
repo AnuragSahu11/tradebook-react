08861e79a6293dcdc9aa5923227264ca7c5bdb83
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-05-05 16:48:18 +0530

    [feat]Error page complete

--- src/components/error-page/error-page.jsx
+++ src/components/error-page/error-page.jsx
@@ -1,8 +1,9 @@
 const ErrorPage = () => {
   return (
     <div className="error-page width-100">
-      <div className="error-image-wrapper center-x width-70"></div>
-      <img className="" src="./images/404Error.svg" alt="" />
+      <div className="error-image-wrapper center-x">
+        <img className="width-100" src="./images/404Error.svg" alt="" />
+      </div>
     </div>
   );
 };
