5877e6e944dae2e1ba281f9b9eca4943da15a184
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-05-21 13:07:10 +0530

    [fix]toastify fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import "./ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import {
   LandingPage,
